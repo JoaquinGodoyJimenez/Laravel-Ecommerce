078071402851521979e76d078e5e197b64236315
Joaquin Godoy Jimenez <19030394@itcelaya.edu.mx>
2023-10-03 09:46:34 -0600

    Layout and initial pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ecommerce.index');
+});
+
+Route::get('/cart', function () {
+    return view('ecommerce.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('ecommerce.checkout');
+});
+
+Route::get('/contact', function () {
+    return view('ecommerce.contact');
+});
+
+Route::get('/login', function () {
+    return view('ecommerce.login');
+});
+
+Route::get('/account', function () {
+    return view('ecommerce.myaccount');
+});
+
+Route::get('/productdetail', function () {
+    return view('ecommerce.productdetail');
+});
+
+Route::get('/productlist', function () {
+    return view('ecommerce.productlist');
+});
+
+Route::get('/wishlist', function () {
+    return view('ecommerce.wishlist');
 });
